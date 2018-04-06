c7394b7ef28fd9d290464aebac821132398b7c06
Pham Nguyen Khoi <khoipham2016@gmail.com>
2018-04-06 12:50:53 +0800

    Create Room class

--- eclipse-workspace/oodp-assignment/src/room/Room.java
+++ eclipse-workspace/oodp-assignment/src/room/Room.java
@@ -1,5 +1,111 @@
 package room;
 
-public class Room {
+import java.io.Serializable;
 
+public class Room implements Serializable{
+	private String roomNo;
+	private String roomType;
+	private String bedType;
+	private boolean wifi;
+	private String view;
+	private boolean smoking;
+	private String status;
+	
+	public Room(String roomNo, String roomType, String bedType, boolean wifi, String view, boolean smoking, String status) {
+		super();
+		this.roomType = roomType;
+		this.roomNo = roomNo;
+		this.bedType = bedType;
+		this.wifi = wifi;
+		this.view = view;
+		this.smoking = smoking;
+		this.status = status;
+	}
+	
+	public String getRoomType() {
+		return roomType;
+	}
+	
+	public String getRoomNo() {
+		return roomNo;
+	}
+
+	public String getBedType() {
+		return bedType;
+	}
+
+	public boolean getWifi() {
+		return wifi;
+	}
+
+	public String getView() {
+		return view;
+	}
+	
+	public boolean getSmoking() {
+		return smoking;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+
+	public void setRoomType(String roomType) {
+		this.roomType = roomType;
+	}
+	
+	public void setRoomNo(String roomNo) {
+		this.roomNo = roomNo;
+	}
+	
+	public void setBedType(String bedType) {
+		this.bedType = bedType;
+	}
+	
+	public void setWifi(boolean wifi) {
+		this.wifi = wifi;
+	}
+	
+	public void setView(String view) {
+		this.view = view;
+	}
+	
+	public void setSmoking(boolean smoking) {
+		this.smoking = smoking;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String hashCode() {
+		// TODO Auto-generated method stub
+		return this.roomNo;
+	}
+
+	/**
+	 * @return true if passport is the same
+	 */
+	public boolean equals(String roomNo) {
+		return (roomNo.equals(this.roomNo));
+	}
+
+	/**
+	 * @return true if room entity is same else false
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return (obj instanceof Room) ? (((Room) obj).getRoomNo().equalsIgnoreCase(this.roomNo)) : false;
+	}
+
+	/**
+	 * @return string
+	 */
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return String.format("Room number: %s\nRoom type: %s\nBed type: %s\nWifi: %s\nView: %s\nSmoking: %s\nStatus: %s", roomNo, roomType, bedType, wifi ? "yes" : "no", view, smoking ? "allowed" : "not allowed", status);
+	}
 }
