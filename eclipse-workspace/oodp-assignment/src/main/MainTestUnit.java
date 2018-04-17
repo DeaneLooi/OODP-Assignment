66dc201d6a73337c2cbf2384b9eb34697772b28a
Low Shu En <Low Shu En@192.168.1.35>
2018-04-18 00:08:56 +0800

    Edited code for breakdown of days

--- eclipse-workspace/oodp-assignment/src/main/MainTestUnit.java
+++ eclipse-workspace/oodp-assignment/src/main/MainTestUnit.java
@@ -1,6 +1,11 @@
 package main;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,10 +14,11 @@ import guest.GuestController;
 import room.Room;
 import room.RoomController;
 import utils.Constants;
+import utils.RegexValidation;
 
 class MainTestUnit {
 
-	@Test
+	/*@Test
 	void testRoom() {
 		
 		//MainController.createRoom();
@@ -75,6 +81,76 @@ class MainTestUnit {
 		
 		MainController.checkIn();
 		MainController.checkOut();
+	}*/
+	
+	@Test
+	void printWeekDaysAndWeekEnds() {
+		Scanner input = new Scanner(System.in);
+		String checkin; 
+		String checkout;
+		Date checkInDate = null;
+		Date checkOutDate = null;
+		
+		do {
+			System.out.print("Check-in Date (dd/mm/yyyy) Time: 11 AM: ");
+			while (!input.hasNext()) {
+				System.out.println("Wrong input");
+				input.next();
+			}
+			checkin = input.next();
+			input.nextLine();
+			checkin = checkin.replaceAll(RegexValidation.whiteSpaceRegex, "");
+			if (!checkin.matches(RegexValidation.dateRegex))
+				System.out.println("Wrong date format");
+		} while (!checkin.matches(RegexValidation.dateRegex));
+
+		do {
+			System.out.print("Check-out Date (dd/mm/yyyy) Time: 9 AM: ");
+			while (!input.hasNext()) {
+				System.out.println("Wrong input");
+				input.next();
+			}
+			checkout = input.next();
+			input.nextLine();
+			checkout = checkout.replaceAll(RegexValidation.whiteSpaceRegex, "");
+			if (!checkout.matches(RegexValidation.dateRegex))
+				System.out.println("Wrong date format");
+		} while (!checkout.matches(RegexValidation.dateRegex));
+		checkin = checkin + " 11:00:00";
+		checkout = checkout + " 09:00:00";
+		if (null != checkin && checkin.trim().length() > 0 && null != checkout && checkout.trim().length() > 0) {
+			SimpleDateFormat myFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+			try {
+				checkInDate = myFormat.parse(checkin);
+				// checkInDate.setHours(11);
+				checkOutDate = myFormat.parse(checkout);
+				// checkOutDate.setHours(9);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		int weekend = 0;
+		
+		//long checkindate = checkInDate.getTime();
+		//long checkoutdate = checkOutDate.getTime();
+		long diffInMillies = Math.abs(checkOutDate.getTime() - checkInDate.getTime());
+		long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS) + 1;
+		
+		Date startDate = checkInDate;
+		for(int i=0; i<= diff;i++)
+		{
+			if(startDate.getDay() == 0 || startDate.getDay() == 6) {
+				weekend++;
+			}
+			startDate.setDate(startDate.getDate()+1);
+		}
+		
+		System.out.println("this" + diff);
+		int weekday = (int) (diff - weekend);
+		System.out.println("Weekends: " + weekend);
+		System.out.println("Weekdays: " + weekday);
+		
 	}
 	
 
